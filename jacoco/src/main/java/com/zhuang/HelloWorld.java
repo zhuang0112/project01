9d9a1f5b7080b363e029dcb601a2e831d4a361ee
mrzhuang <862627527@qq.com>
2022-04-01 17:41:39 +0800

    第一次提交

---
+++ jacoco/src/main/java/com/zhuang/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.zhuang;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public void hello(){
+        System.out.println("你好啊！");
+    }
+
+
+
+}
+
+
+
---
+++ jacoco/src/test/java/com/zhuang/helloTest.java
@@ -0,0 +1,14 @@
+package com.zhuang;
+
+import com.zhuang.HelloWorld;
+import org.junit.Test;
+
+public class helloTest {
+
+
+    @Test
+    public void test01(){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.hello();
+    }
+}
